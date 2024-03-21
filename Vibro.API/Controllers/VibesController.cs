a2744670f95fe2c651f4ab981d107271b0e02e38
angel <angel@RAZ>
2024-03-21 15:10:07 -0700

    Add project files.

---
+++ Vibro.API/Controllers/VibesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vibro.API.Controllers
+{
+    // https://localhost:portnumber/api/vibes
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VibesController : ControllerBase
+    {
+        // GET: https://localhost:portnumber/api/vibes
+        [HttpGet]
+        public IActionResult GetAllVibes()
+        {
+            string[] vibeNames = 
+        }
+    }
+}
