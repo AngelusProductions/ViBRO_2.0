967e914382c40b3decc2442c7ef3dc06df0fc12b
Corey Angelus <angelusproductions@gmail.com>
2024-04-30 20:18:31 -0700

    Add Entity Framework

---
+++ Vibro.API2/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vibro.API2.Entities;
+
+namespace Vibro.API2.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+}
