be41a3a54b615d27f150906626b70432287ce8fd
Corey Angelus <angelusproductions@gmail.com>
2024-05-06 21:41:33 -0700

    Add Users Call

--- Vibro.API2/Program.cs
+++ Vibro.API2/Program.cs
@@ -8,12 +8,14 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseCors(builder => builder
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
--- Vibro.Client/src/app/app.component.ts
+++ Vibro.Client/src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Vibro.Client';
+  users: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5000/api/users').subscribe({
+      next: response => this.users = response,
+      error: err => console.error(err),
+      complete: () => console.log('Request Completed')
+    });
+  }
 }
