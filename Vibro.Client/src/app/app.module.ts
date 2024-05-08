2f4a16f2f0188f639281fb770bb0424b7dee7ec5
Corey Angelus <angelusproductions@gmail.com>
2024-05-07 20:22:08 -0700

    Revert "Configure SSL"
    This reverts commit 2ded04e96a11162df2691ab816619182e9b79454.

--- Vibro.Client/src/app/app.module.ts
+++ Vibro.Client/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -13,8 +12,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    BrowserAnimationsModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
